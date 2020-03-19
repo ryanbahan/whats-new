1030136d156104188708602e61be86008c79da87
Ryan Bahan <ryanbahan@Ryans-iMac.local>
2020-03-19 17:23:57 -0600

    Updates

--- precache-manifest.11cdbb0dc091e7bec99dcaab8269bbe8.js
+++ precache-manifest.11cdbb0dc091e7bec99dcaab8269bbe8.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "99f3f0672288749b8b19d52bb8c61827",
+    "revision": "821cefbaf6fbc554ab5b2e6d9364f7ab",
     "url": "/whats-new/index.html"
   },
   {
-    "revision": "adbbc60874b2d0633d57",
+    "revision": "e85f6a8fb27ea521df13",
     "url": "/whats-new/static/css/main.6351e955.chunk.css"
   },
   {
-    "revision": "e9db0af654094baf1cf1",
-    "url": "/whats-new/static/js/2.3366c6b6.chunk.js"
+    "revision": "9146a7549a687dbed07e",
+    "url": "/whats-new/static/js/2.101f00d0.chunk.js"
   },
   {
     "revision": "f081b2f2e58926dde75eece0c48e731e",
-    "url": "/whats-new/static/js/2.3366c6b6.chunk.js.LICENSE.txt"
+    "url": "/whats-new/static/js/2.101f00d0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "adbbc60874b2d0633d57",
-    "url": "/whats-new/static/js/main.a06c9d91.chunk.js"
+    "revision": "e85f6a8fb27ea521df13",
+    "url": "/whats-new/static/js/main.e4cd7a6e.chunk.js"
   },
   {
     "revision": "49721edf61db0b477837",
