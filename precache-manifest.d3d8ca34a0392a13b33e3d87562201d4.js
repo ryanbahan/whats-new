629f85c4bb57eee7eb38a57e4afffed0c516724b
Ryan Bahan <ryanbahan@Ryans-iMac.local>
2020-03-19 13:41:26 -0600

    Updates

---
+++ precache-manifest.d3d8ca34a0392a13b33e3d87562201d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1fcd1044416cd714b098d50d34f4dda",
+    "url": "/whats-new/index.html"
+  },
+  {
+    "revision": "6e53ce33f0c8a83ba670",
+    "url": "/whats-new/static/css/main.3b92672b.chunk.css"
+  },
+  {
+    "revision": "9814b4d49440f0493bd7",
+    "url": "/whats-new/static/js/2.710ad2c0.chunk.js"
+  },
+  {
+    "revision": "f081b2f2e58926dde75eece0c48e731e",
+    "url": "/whats-new/static/js/2.710ad2c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e53ce33f0c8a83ba670",
+    "url": "/whats-new/static/js/main.2146562f.chunk.js"
+  },
+  {
+    "revision": "49721edf61db0b477837",
+    "url": "/whats-new/static/js/runtime-main.bcd50b86.js"
+  }
+]);
